3cc9337972863c1f580f6553e0ff1ab4ac258dc9
land0r <wp.netlog@gmail.com>
2024-12-22 21:41:07 +0100

    fix: deprecation warning for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,7 @@ import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 import cleanCSS from "gulp-clean-css";
 import uglify from "gulp-uglify";
-import concat from "gulp-concat";
 import autoprefixer from "gulp-autoprefixer";
-import sourcemaps from "gulp-sourcemaps";
 import browserSync from "browser-sync";
 
 // Initialize gulp-sass with Dart Sass
@@ -29,11 +27,9 @@ const paths = {
 export function styles() {
 	return gulp
 		.src(paths.styles.src)
-		.pipe(sourcemaps.init())
 		.pipe(sass().on("error", sass.logError))
 		.pipe(autoprefixer({ cascade: false }))
 		.pipe(cleanCSS())
-		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest(paths.styles.dest))
 		.pipe(browserSync.stream());
 }
@@ -41,11 +37,8 @@ export function styles() {
 export function scripts() {
 	return gulp
 		.src(paths.scripts.src)
-		.pipe(sourcemaps.init())
-		.pipe(concat("main.js"))
-		.pipe(uglify())
-		.pipe(sourcemaps.write("."))
 		.pipe(gulp.dest(paths.scripts.dest))
+		.pipe(uglify())
 		.pipe(browserSync.stream());
 }
 
